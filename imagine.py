330c05b4265c29abb5e63503256f0e1c00f26240
Daniel Waardal <waawal@boom.ws>
2014-01-04 22:04:30 +0100

    adding all the handlers

--- imagine.py
+++ imagine.py
@@ -5,17 +5,36 @@ import tornado.web
 from tornado.options import define, parse_command_line
 
 
-define("port", default=5000, help="Can I haz port?", type=int)
-
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Imagine this!")
 
+
+class ResizeHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Imagine this!")
+
+
+class CropHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Imagine this!")
+
+
+class MagicHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Imagine this!")
+
+
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/resize/", ResizeHandler),
+    (r"/crop/", CropHandler),
+    (r"/magic/", MagicHandler),
 ])
 
+
 if __name__ == "__main__":
+    define("port", default=5000, help="Can I haz port?", type=int)
     parse_command_line()
     application.listen(tornado.options.options.port)
     tornado.ioloop.IOLoop.instance().start()
